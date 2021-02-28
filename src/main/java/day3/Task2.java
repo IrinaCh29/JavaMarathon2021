79851a4c0c35f5a003fc64ff168d34a9e2f9cd71
Irina Chub <irina.chub29@gmail.com>
2021-02-28 20:14:21 +0200

    complete day3 tasks

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,17 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        for (; ; ) {
+            double dividend = scanner.nextDouble(); //делимое
+            double divider = scanner.nextDouble();  //делитель
+            if (divider == 0)
+                break;
+            System.out.println(dividend / divider);
+        }
     }
 }
