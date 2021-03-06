e755f3c8f088dc9b4a5316d4ee4776f2fd95a131
Irina Chub <irina.chub29@gmail.com>
2021-03-06 21:34:34 +0200

    complete day8 tasks

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,30 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        long startString = System.currentTimeMillis();
+        String numbers = "";
 
+        for (int i = 0; i <= 20000; i++) {
+            numbers += i + " ";// OR = numbers + i + " ";
+        }
+        long finishString = System.currentTimeMillis();
+
+        System.out.println(numbers);
+        long timeString = finishString - startString;
+        System.out.println("Длительность выполнения цикла, когда класс String: " + timeString);
+
+        long startSB = System.currentTimeMillis();
+
+        StringBuilder stringBuilderNumbers = new StringBuilder("");
+
+        for (int i = 0; i <= 20000; i++) {
+            stringBuilderNumbers.append(i).append(" ");
+        }
+
+        long finishSB = System.currentTimeMillis();
+
+        System.out.println(stringBuilderNumbers);
+        long timeStringBuilder = finishSB - startSB;
+        System.out.println("Длительность выполнения цикла, когда класс StringBuilder: " + timeStringBuilder);
     }
 }
