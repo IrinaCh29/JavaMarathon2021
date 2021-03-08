0f540c838e6f61dee20cdb0062563e1d7087c7d3
Irina Chub <irina.chub29@gmail.com>
2021-03-08 23:03:19 +0200

    complete day13 tasks

--- src/main/java/day13/Task1.java
+++ src/main/java/day13/Task1.java
@@ -2,6 +2,28 @@ package day13;
 
 public class Task1 {
     public static void main(String[] args) {
+        User user1 = new User("Ирина");
+        User user2 = new User("Марина");
+        User user3 = new User("Карина");
 
+        user1.sendMessage(user2, "Привет! Меня зовут Ирина :-)");
+        user1.sendMessage(user2, "Как у тебя дела?");
+
+        user2.sendMessage(user1, "Привет!");
+        user2.sendMessage(user1, "А я Марина :-)");
+        user2.sendMessage(user1, "Все ок, спасибо :)");
+
+        user3.sendMessage(user1, "Здравствуй!");
+        user3.sendMessage(user1, "Карина");
+        user3.sendMessage(user1, "Как у тебя дела?");
+
+        user1.sendMessage(user3, "Приятно познакомиться!");
+        user1.sendMessage(user3, "Мне тоже!");
+        user1.sendMessage(user3, "Пройдем пообщаемся?");
+
+        user3.sendMessage(user1, "Конечно!");
+
+        MessageDatabase.showDialog(user1,user3);
+        MessageDatabase.showDialog(user1, user2);
     }
 }
